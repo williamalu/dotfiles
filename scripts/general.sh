8f318c7d218f5879c349f878435c03361f539c60
Shane Kelly <skelly.olin@gmail.com>
2017-09-02 00:14:17 -0400

    Add mountpublic alias and necessary dependencies

--- scripts/general.sh
+++ scripts/general.sh
@@ -3,6 +3,11 @@ echo "--- Fixing wifi dropping issues ---"
 echo "options iwlwifi 11n_disable=1" | sudo tee /etc/modprobe.d/iwlwifi-11n_disable.conf
 echo ""
 
+# For manually mounting CIFS/SMB Windows Filesystems
+echo "--- Installing cifs-utils ---"
+sudo apt-get install cifs-utils
+echo ""
+
 echo "--- System reboot may be required for changes to take affect ---"
 echo ""
 
