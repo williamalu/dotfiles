37e11d8641a41e7416320ad5c0295e16d9a5c2d1
Shane Kelly <skelly.olin@gmail.com>
2017-09-02 14:56:29 -0400

    Add nvm and necessary dependencies

--- scripts/general.sh
+++ scripts/general.sh
@@ -8,6 +8,17 @@ echo "--- Installing cifs-utils ---"
 sudo apt-get install cifs-utils
 echo ""
 
+echo "--- Downloading NVM ---"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | zshpick
+echo ""
+
+echo "--- Installing NVM ---"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
+echo ""
+
 echo "--- System reboot may be required for changes to take affect ---"
 echo ""
 
