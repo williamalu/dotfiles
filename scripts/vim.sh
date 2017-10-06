46f6eafefb45e28788d173792bce3da9c8a4f187
Shane Kelly <skelly.olin@gmail.com>
2017-10-05 20:08:42 -0400

    Add auto-linting for lots of file types

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,6 +2,15 @@ echo "--- Installing vim ---"
 sudo apt-get -y install vim
 echo ""
 
+echo "--- Installing pip ---"
+sudo apt-get -y install python-pip
+echo ""
+
+# Python linter
+echo "--- Installing flake8 ---"
+pip install flake8
+echo ""
+
 echo "--- Updating vimrc ---"
 mkdir -p ~/.vim
 cp -v ../config/vimrc ~/.vim/vimrc
