c80ff1459ab6b277b9508f5976b71bde1bc56232
Shane Kelly <skelly.olin@gmail.com>
2017-12-02 20:20:12 -0500

    Add vim cpplinter

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -6,9 +6,14 @@ echo "--- Installing pip ---"
 sudo apt-get -y install python-pip
 echo ""
 
+# C++ linter
+echo "--- Installing cpplint ---"
+sudo pip install cpplint
+echo ""
+
 # Python linter
 echo "--- Installing flake8 ---"
-pip install flake8
+sudo pip install flake8
 echo ""
 
 echo "--- Updating vimrc ---"
