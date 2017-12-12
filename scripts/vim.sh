ddc9c42584a3c9f8160ce8e5c714a9ce09cab347
Shane Kelly <skelly.olin@gmail.com>
2017-12-12 13:16:16 -0500

    Add vim fuzzy file finder/opener

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,23 +2,29 @@ echo "--- Installing vim ---"
 sudo apt-get -y install vim
 echo ""
 
+# For installing other things
 echo "--- Installing pip ---"
 sudo apt-get -y install python-pip
 echo ""
 
-# C++ linter
+# C++ linter for w0rp/ale plugin
 echo "--- Installing cpplint ---"
 sudo pip install cpplint
 echo ""
 
-# C++ linter
+# C++ linter for w0rp/ale plugin
 echo "--- Installing cppcheck ---"
 sudo apt-get install cppcheck
 echo ""
 
-# Python linter
+# Python linter for w0rp/ale plugin
 echo "--- Installing flake8 ---"
 sudo pip install flake8
+echo ""
+
+# Fuzzy file finder for junegunn/fzf plugin
+echo "--- Installing ag ---"
+sudo apt-get install silversearcher-ag
 echo ""
 
 echo "--- Updating vimrc ---"
