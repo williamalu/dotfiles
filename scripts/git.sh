46391b845d80561dba183c5ec9314695f001dd03
Shane Kelly <shaneqkelly@gmail.com>
2018-04-07 15:31:40 -0400

    Update git email address

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install git
 echo ""
 
 echo "--- Editing git config ---"
-git config --global user.email "skelly.olin@gmail.com"
+git config --global user.email "shaneqkelly@gmail.com"
 git config --global user.name "Shane Kelly"
 git config --global core.editor "vi"
 git config --global push.default "simple"
